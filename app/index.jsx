b3d6c29f058043c6108e228a51a1e7e90bc19a3b
Lee Samuel <psalms016@gmail.com>
2015-10-12 20:33:18 +1300

    Alt Flux update

--- app/index.jsx
+++ app/index.jsx
@@ -3,13 +3,17 @@ import './main.css';
 
 import React from 'react';
 import App from './components/App.jsx';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 
 main();
 
 function main() {
-	const app = document.createElement('div');
+  persist(alt, storage, 'app');
+  const app = document.createElement('div');
 
-	document.body.appendChild(app);
+  document.body.appendChild(app);
 
-	React.render(<App />, app);
+  React.render(<App />, app);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,85 +2,55 @@ var path = require('path');
 var HtmlwebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 var merge = require('webpack-merge');
-var Clean = require('clean-webpack-plugin');
 
 var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+
+process.env.BABEL_ENV = TARGET;
 
 var common = {
-	entry: path.resolve(ROOT_PATH, 'app'),
-	resolve: {
-		extensions: ['', '.js', '.jsx']
-	},
-	output: {
-		path: path.resolve(ROOT_PATH, 'build'),
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.css$/,
-				loaders: ['style', 'css'],
-				include: path.resolve(ROOT_PATH, 'app')
-			}
-		]
-	},
-	plugins: [
-		new HtmlwebpackPlugin({
-			title: 'Kanban app'
-		})
-	]
+  entry: APP_PATH,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: APP_PATH
+      },
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        include: APP_PATH
+      }
+    ]
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Kanban app'
+    })
+  ]
 };
 
-if (TARGET === 'start' || !TARGET) {
-	module.exports = merge(common, {
-		devtool: 'eval-source-map',
-		module: {
-			loaders: [
-				{
-					test: /\.jsx?$/,
-					loaders: ['react-hot', 'babel'],
-					include: path.resolve(ROOT_PATH, 'app')
-				}
-			]
-		},
-		devServer: {
-		    historyApiFallback: true,
-		    hot: true,
-		    inline: true,
-		    progress: true,
-		    port: 4000
-		},
-		plugins: [
-			new webpack.HotModuleReplacementPlugin()
-		]
-	});
-}
-
-if(TARGET === 'build') {
-	module.exports = merge(common, {
-		devtool: 'source-map',
-		module: {
-			loaders: [
-			    {
-					test: /\.jsx?$/,
-					loaders: ['babel'],
-					include: path.resolve(ROOT_PATH, 'app')
-			    }
-			]
-		},
-		plugins: [
-			new Clean(['build']),
-			new webpack.optimize.UglifyJsPlugin({
-				compress: {
-					warnings: false
-				}
-			})
-			// new webpack.DefinePlugin({
-			// 	'process.env': {
-			// 		'NODE_env': JSON.stringify('production')
-			// 	}
-			// })
-		]
-	});
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
